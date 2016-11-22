6b0ceb6127067e0742244466bc8615e6432e23a8
Timothy Haskins <tijohask@iupui.edu>
2016-11-22 00:22:57 -0500

    Program now prints name and relevant state elements.

--- getLights.py
+++ getLights.py
@@ -6,6 +6,7 @@ import requests
 import json
 
 url = requests.get("https://www.meethue.com/api/nupnp").text
+# [{"id":"001788fffe222a8f","internalipaddress":"192.168.2.140"}]
 urldata = json.loads(url)
 address = urldata[0]["internalipaddress"]
 
@@ -16,9 +17,16 @@ f.close()
 
 print( "http://" + address + "/api/" + username + "/lights" )
 address = "http://" + address + "/api/" + username + "/lights"
-# req = requests.post(address, json=data)
-# req = requests.put(address, json=NewData)
-req = requests.get(address)
-print( req.text )
 
+req = requests.get( address )
+data = json.loads(req.text)
+print( json.dumps(data, indent=4, sort_keys=True) )
 
+for item in data:
+	print( )
+	print( data[item]["name"] )
+	print( "Reachable: " + str(data[item]["state"]["reachable"]) )
+	print( "On: " + str(data[item]["state"]["on"]) )
+
+#if( data["5"]["state"]["on"] ):
+#	print("True")
