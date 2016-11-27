0db61ae6b4ffb7ba6fff1d2e69ddf83715db2284
Timothy Haskins <tijohask@iupui.edu>
2016-11-27 14:34:25 -0500

    Minor formatting changes.

--- getLights.py
+++ getLights.py
@@ -5,28 +5,28 @@
 import requests
 import json
 
-url = requests.get("https://www.meethue.com/api/nupnp").text
+url = requests.get( "https://www.meethue.com/api/nupnp" ).text
 # [{"id":"001788fffe222a8f","internalipaddress":"192.168.2.140"}]
-urldata = json.loads(url)
+urldata = json.loads( url )
 address = urldata[0]["internalipaddress"]
 
-f = open("Username.txt", "r")
+f = open( "Username.txt", "r" )
 f.readline()
 username = f.readline().rstrip()
 f.close()
 
-print( "http://" + address + "/api/" + username + "/lights" )
-address = "http://" + address + "/api/" + username + "/lights"
+#print( "http://" + address + "/api/" + username + "/lights/" )
+address = "http://" + address + "/api/" + username + "/lights/"
 
 req = requests.get( address )
-data = json.loads(req.text)
-print( json.dumps(data, indent=4, sort_keys=True) )
+data = json.loads( req.text )
+print( json.dumps( data, indent=4, sort_keys=True ) )
 
 for item in data:
 	print( )
 	print( data[item]["name"] )
-	print( "Reachable: " + str(data[item]["state"]["reachable"]) )
-	print( "On: " + str(data[item]["state"]["on"]) )
+	print( "Reachable: " + str( data[item]["state"]["reachable"] ) )
+	print( "On: " + str( data[item]["state"]["on"] ) )
 
 #if( data["5"]["state"]["on"] ):
 #	print("True")
